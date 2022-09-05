5cac9441ca6d2f7c50d3e2b5f38b1ff98553f5d7
Zachary Youngdale <zyoungdale@gmail.com>
2022-09-05 12:05:23 -0400

    commit tree 5605d0ab98eab9fff24dc03c6d9760f81ad8c0a8

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((res) => res.json())
+    .then((data) => renderBooks(data));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
